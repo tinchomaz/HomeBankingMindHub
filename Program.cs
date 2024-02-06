f6b97a8562a4e9fead001d9fd510e376ab61189c
Martin Flores <juanmartinf7@hotmail.com>
2024-02-06 10:54:19 -0300

    arreglo de los $values

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<HomeBankingContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion")));
 
 builder.Services.AddControllers().AddJsonOptions(x =>
-    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddDbContext<HomeBankingContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion")));
 
